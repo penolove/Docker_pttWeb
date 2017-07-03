63841081119ca5977ad76619d007fbf289da2bd2
stream <penolove19.am97@g2.nctu.edu.tw>
2017-07-03 23:43:23 +0800

    backup/restore done

--- run_pttweb.sh
+++ run_pttweb.sh
@@ -1,3 +1,3 @@
 docker run -ti --shm-size="4g" --name ptt_core --net host \
-           -d penolove/ubuntu:PttWeb_parser_201707
+           -d penolove/ubuntu:PttWeb_parback_201707
 #           -d penolove/ubuntu:pure_postgresql \
--- run_pttweb_restoreDB.sh
+++ run_pttweb_restoreDB.sh
@@ -1,6 +1,4 @@
 docker run -ti --shm-size="4g" --name ptt_core --net host \
 -v $(pwd)/dataforupdate/:/home/stream/D4U \
--d penolove/ubuntu:PttWeb_parser_201707
+-d penolove/ubuntu:PttWeb_parback_201707
 
-# here for update outfile
-#-v $(pwd)/dataforupdate/outfile:/var/lib/postgresql/outfile \
