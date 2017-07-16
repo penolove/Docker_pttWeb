84da212e4c4bca4a7afd66ab134625cacc0004b0
andy <penolove15@gmail.com>
2017-07-16 21:11:21 +0800

    edit replace.sh

--- dataforupdate/orders/replace.sh
+++ dataforupdate/orders/replace.sh
@@ -1,4 +1,4 @@
-cp /home/stream/D4U/starbucks.sqlite /home/stream/Documents/minimum_django/starbucks.sqlite;
+cp /home/stream/D4U/starbucks.sqlite /home/stream/Documents/minimum_django/pttWeb/starbucks.sqlite;
 cp /home/stream/D4U/images.tar.gz /home/stream/Documents/minimum_django/pttWeb/static/;
 cd /home/stream/Documents/minimum_django/pttWeb/static/;
 tar -zxf images.tar.gz ;
