923582195c8022e3980588c3ef5bc5dfe14d67c4
andy <penolove15@gmail.com>
2017-08-03 17:00:50 +0800

    edit backup/restore script

--- dataforupdate/orders/backup.sh
+++ dataforupdate/orders/backup.sh
@@ -1,4 +1,6 @@
 cp /home/stream/Documents/minimum_django/pttWeb/starbucks.sqlite /home/stream/D4U/;
+cp Documents/ptt_web_oop/gossip/weekdatetemp.txt D4U/weekdatetemp.txt
+cp Documents/ptt_web_oop/gossip/datetemp.txt D4U/datetemp.txt
 cd /home/stream/Documents/minimum_django/pttWeb/static;
 tar -czf /home/stream/D4U/images.tar.gz topicmodel/ worldcloud/
 export PATH=/usr/lib/postgresql/9.5/lib/:$PATH
--- dataforupdate/orders/replace.sh
+++ dataforupdate/orders/replace.sh
@@ -1,6 +1,8 @@
 cp /home/stream/D4U/starbucks.sqlite /home/stream/Documents/minimum_django/pttWeb/starbucks.sqlite;
 cp /home/stream/D4U/images.tar.gz /home/stream/Documents/minimum_django/pttWeb/static/;
 cd /home/stream/Documents/minimum_django/pttWeb/static/;
+cp ~/D4U/weekdatetemp.txt ~/Documents/ptt_web_oop/gossip/weekdatetemp.txt 
+cp ~/D4U/datetemp.txt ~/Documents/ptt_web_oop/gossip/datetemp.txt 
 tar -zxf images.tar.gz ;
 rm /home/stream/Documents/minimum_django/pttWeb/static/images.tar.gz;
 
